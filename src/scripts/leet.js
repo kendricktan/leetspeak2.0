9b8ccde322dba0e06aa1cbcbb8d4719319729a45
Kendrick Tan <kendricktan0814@gmail.com>
2017-11-18 11:28:46 +1000

    replaces 'a'

--- src/scripts/leet.js
+++ src/scripts/leet.js
@@ -1,13 +1,27 @@
-/*
-Just draw a border round the document.body.
-*/
+var replacements = {
+    'a': ['á', 'ă']
+}
 
-function printKey(e) {
-    console.log(this.value)
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
+}
+
+function leetspeakv2(e) {    
+    this.value = this.value.split('').map((x) => {
+        var r = replacements[x]
+
+        if (r !== undefined) {
+            return r[getRandomInt(0, r.length)]
+        }
+
+        return x
+    }).join('')
 }
 
 var inputs = document.getElementsByTagName('input')
 
 for (var i = 0; i < inputs.length; i++) {
-    inputs[i].addEventListener('input', printKey)
+    inputs[i].addEventListener('input', leetspeakv2)
 }
